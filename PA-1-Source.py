2e45fbede14760669e5fe13d3283939a71b1c034
Robert <kauldar@users.noreply.github.com>
2019-01-18 14:46:58 -0600

    Small updates.  Going creating .csv insead of .xlsx
    Using the version that creates .csv's.  Removed unessessay imports to help speed up the exectuable.

--- PA-1-Source.py
+++ PA-1-Source.py
@@ -2,22 +2,56 @@ import os
 import random
 import timeit
 import statistics
-import openpyxl
-from openpyxl import load_workbook
-from openpyxl.styles import Font
-from colorama import init
-from termcolor import colored
+
+ARRAY_SIZE = 200  # 100 sets of pairs
+RAND_MIN = 1  # Min value of randomly generated number
+RAND_MAX = 999  # Max value of randomly generated number
 
 
-"""
-Outputs to .xlsx file
-"""
+# Outputs results of algorithm time execution tests to .csv file.
+def main():
+    # Array of size 200, with values ranging from 1 to 999
+    # Values are used in pairs, so an array of size 200 will produce 100 test runs
+    print("\nStarting Euclid Tests\n")
+
+    # Creates a directory on the host in the current path called PA-1-Results, if it did not previously
+    # exist.  The output files will be written here.  Any prexisting output files will be overwritten
+    if not os.path.exists("PA-1-Results"):
+        print("Creating directory PA-1-Results ... ", end="")
+        os.mkdir("PA-1-Results")
+        print("Done\n")
+    os.chdir("PA-1-Results")
+
+    print("Creating random inputs from " + str(RAND_MIN) + " to " + str(RAND_MAX) + " ... ", end="")
+    numbers = create_random_array(ARRAY_SIZE, RAND_MIN, RAND_MAX)
+
+    # Brute Force starting from 'b'
+    print("Done\nRunning Brute Force ... ", end="")
+    output_to_csv("Brute_Force", run_test(brute_force, numbers))
+
+    # Brute Force starting from 1
+    print("Done\nRunning Brute Force ... ", end="")
+    output_to_csv("Brute_Force_Second", run_test(brute_force_second, numbers))
+
+    # Euclid no Subtraction
+    print("Done\nRunning Original Euclid ... ", end="")
+    output_to_csv("Original_Euclid", run_test(original_euclid, numbers))
+
+    # Euclid with Subtraction
+    print("Done\nRunning Second Euclid ... ", end="")
+    output_to_csv("Second_Euclid", run_test(second_euclid, numbers))
+
+    print("Done\n\nResults saved to " + os.getcwd())
+
+    # Added so the console that appears when the executable is run doesn't dissappear automatically
+    input("Press Enter to exit ")
 
 
 class DataPoint(object):
     """
     Contains the information about each test run to use in results
     """
+
     def __init__(self, value1, value2, gcd, time):
         """
         :param value1: First value from random array pair
@@ -26,7 +60,7 @@ class DataPoint(object):
         :type: integer
         :param gcd: Calculated greatest comman divisor
         :type: integer
-        :param time: Time ellapsed during GCD calculation
+        :param time: Time elapsed during GCD calculation
         :type: float
         """
         self.value1 = value1
@@ -39,12 +73,19 @@ class DataPoint(object):
 # If a number is a divisor of b, check if its also a divisor of 'a'
 # Note 'b' is always <= a
 def brute_force(a, b):
-    i = b
-    for n in range(1, b):
-        if a % i == 0 and b % i == 0:
-            return i
-        i -= 1
+    for n in reversed(range(1, b + 1)):
+        if a % n == 0 and b % n == 0:
+            return n
     return 1
+
+
+# Starts iterating from 1 to 'b'
+def brute_force_second(a, b):
+    gcd = 1
+    for n in range(1, b + 1):
+        if a % n == 0 and b % n == 0:
+            gcd = n
+    return gcd
 
 
 def original_euclid(a, b):
@@ -72,8 +113,13 @@ def second_euclid(a, b):
     return a
 
 
-def run_test(function, length, number_array):
-    datapoints = []  # Array of Datapoint objects
+# Runs n/2 GCD calc tests  for the given algorithm, where 'n' is the length of the number_array.
+# Each of the algorithm's test values, the resulting GCD, and the execution time are saved as a
+# DataPoint object and added to the array of test_results.
+def run_test(function, number_array):
+    test_results = []  # Array of Datapoint objects
+    length = len(number_array) // 2
+    number_array = iter(number_array)  # Use an iterator to make pulling pairs of values simple
     for i in range(0, length):
         # Gather random pair
         a = next(number_array)
@@ -93,95 +139,28 @@ def run_test(function, length, number_array):
         stop_time = timeit.default_timer()
 
         # Add results to dataset.  Time is saved in milliseconds
-        datapoints.append(DataPoint(a, b, gcd, (stop_time - start_time) * 1000))
-    return datapoints
+        test_results.append(DataPoint(a, b, gcd, (stop_time - start_time) * 1000))
+    return test_results
 
 
-"""
-Functions to Output Results
-"""
+# Outputs 2 files using the data results array.
+# First is the complete set of all test results for the given algorithm.
+# The second is the overall statistics for that algorithm
+def output_to_csv(alg_name, results):
+    # Complete Results of all GCD Calculations (val1, val2, GCD, and execution time)
+    with open(alg_name + "_Results.csv", 'w') as file:
+        file.write("Number One,Number Two, GCD, Time Spent (Milliseconds)\n")
+        file.write("\n".join(",".join((str(d.value1), str(d.value2), str(d.gcd), str(d.time))) for d in results))
+    # Final Stats of the data (max, min, avg, and median
+    with open(alg_name + "_Statistics.csv", 'w') as file:
+        file.write("Statistics,Milliseconds")
+        file.write(",".join(("\nMaximum Time", str(max(data_point.time for data_point in results)))))
+        file.write(",".join(("\nMinimum Time", str(min(data_point.time for data_point in results)))))
+        file.write(",".join(("\nAverage Time", str(sum(data_point.time for data_point in results) / len(results)))))
+        file.write(",".join(("\nMedian Time", str(statistics.median(data_point.time for data_point in results)))))
 
 
-def output_to_excel(alg_name, data):
-    create_results_xlsx(alg_name, data)
-    create_stats_xlsx(alg_name, data)
-
-
-def create_results_xlsx(alg_name, data):
-    # Create the excel file
-    workbook = openpyxl.Workbook()
-    filename = alg_name + "_Results.xlsx"
-    workbook.save(filename)
-    workbook = load_workbook(filename)
-
-    # Set the active sheet
-    sheet = workbook.active
-    sheet.title = alg_name.replace("_", ' ') + ' Results'
-
-    # Format the column headers
-    sheet.cell(row=1, column=1).value = 'Number 1'
-    sheet.cell(row=1, column=2).value = 'Number 2'
-    sheet.cell(row=1, column=3).value = 'GCD'
-    sheet.cell(row=1, column=4).value = 'Time (ms)'
-    ws = workbook[sheet.title]
-    Font(bold=True, italic=True)
-    for cell in ws["1:1"]:
-        cell.font = Font(bold=True, italic=True)
-
-    # Populate
-    row = 1
-    for data_point in data:
-        row += 1
-        sheet.cell(row=row, column=1).value = data_point.value1
-        sheet.cell(row=row, column=2).value = data_point.value2
-        sheet.cell(row=row, column=3).value = data_point.gcd
-        sheet.cell(row=row, column=4).value = data_point.time
-
-    # Set a uniform width
-    for column_cells in sheet.columns:
-        sheet.column_dimensions[column_cells[0].column].width = 10
-    workbook.save(filename)
-
-
-def create_stats_xlsx(alg_name, data):
-    # Create the excel file
-    workbook = openpyxl.Workbook()
-    filename = alg_name + "_Statistics.xlsx"
-    workbook.save(filename)
-
-    # Set the active sheet
-    workbook = load_workbook(filename)
-    sheet = workbook.active
-    sheet.title = alg_name.replace("_", ' ') + ' Stats'
-
-    # Format Header row/columns
-    sheet.cell(row=1, column=2).font = Font(bold=True)
-    for n in range(1, 6):
-        sheet.cell(row=n, column=1).font = Font(bold=True)
-    sheet.cell(row=1, column=1).value = 'Statistics'
-    sheet.cell(row=1, column=2).value = 'Milliseconds'
-    sheet.cell(row=2, column=1).value = 'Maximum'
-    sheet.cell(row=3, column=1).value = 'Minimum'
-    sheet.cell(row=4, column=1).value = 'Average'
-    sheet.cell(row=5, column=1).value = 'Median'
-
-    # Populate
-    sheet.cell(row=2, column=2).value = max(data_point.time for data_point in data)  # max time
-    sheet.cell(row=3, column=2).value = min(data_point.time for data_point in data)  # min time
-    sheet.cell(row=4, column=2).value = sum(data_point.time for data_point in data) / len(data)  # avg time
-    sheet.cell(row=5, column=2).value = statistics.median(data_point.time for data_point in data)  # median time
-
-    # Set uniform width
-    for column_cells in sheet.columns:
-        sheet.column_dimensions[column_cells[0].column].width = 12
-    workbook.save(filename)
-
-
-"""
-Helpers
-"""
-
-
+# Help function which creates the random array of test inputs
 def create_random_array(array_length, rand_min, rand_max):
     rand = []
     for num in range(array_length):
@@ -189,44 +168,6 @@ def create_random_array(array_length, rand_min, rand_max):
     return rand
 
 
-"""
-Run Program
-"""
-
-
+# Run the script
 if __name__ == "__main__":
-    # Array of size 200, with values ranging from 1 to 999
-    # Values are used in pairs, so an array of size 200 will produce 100 test runs
-    array_size = 200
-    rand_min = 1
-    rand_max = 999
-
-    init()  # Initialize colorama.  Used for colors in the terminal
-    print(colored("\nStarting Euclid Tests\n", 'blue'))
-
-    # Creates a directory on the host in the current path called PA-1-Results, if it did not previously
-    # exist.  The output files will be written here.  Any prexisting output files will be overwritten
-    if not os.path.exists("PA-1-Results"):
-        print("Creating directory PA-1-Results ... ", end="")
-        os.mkdir("PA-1-Results")
-        print(colored("Done\n", 'green'))
-    os.chdir("PA-1-Results")
-
-    print("Creating random inputs from " + str(rand_min) + " to " + str(rand_max) + " ... ", end="")
-    numbers = create_random_array(array_size, rand_min, rand_max)
-    length = len(numbers) // 2
-
-    print(colored("Done", 'green') + "\nRunning Brute Force ... ", end="")
-    output_to_excel("Brute_Force", run_test(brute_force, length, iter(numbers)))
-
-    print(colored("Done", 'green') + "\nRunning Original Euclid ... ", end="")
-    output_to_excel("Original_Euclid", run_test(original_euclid, length, iter(numbers)))
-
-    print(colored("Done", 'green') + "\nRunning Second Euclid ... ", end="")
-    output_to_excel("Second_Euclid", run_test(second_euclid, length, iter(numbers)))
-
-    print(colored("Done\n", 'green'))
-    print(colored("Finished.  ", 'blue') + "Results saved to " + os.getcwd())
-
-    # Added so the console that appears when the executable is run doesn't dissappear automatically
-    input("Press Enter to exit ")
+    main()
